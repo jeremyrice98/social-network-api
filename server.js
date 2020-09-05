0c9b17d58d7a5fc4f5e215b4d32ad8b70e66db5c
jeremyrice98 <ricefamily1003@gmail.com>
2020-09-05 11:41:32 -0600

    add server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const express = require('express');
+// Port
+const app = express();
+const PORT = process.env.PORT || 3001;
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Use Routes
+app.use(require('./routes'));
+// Define Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+// Open Connection
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
